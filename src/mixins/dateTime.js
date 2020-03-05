ab24d72a085a01760b8fab3b62b7943ff90bc7fc
Olli Pirttilä <olli.pirttila@iki.fi>
2020-03-05 22:05:54 +0200

    Add date-fns library for date/time manipulations. Add methods for timedifference of scheduled and actual departure times from stop. Minor style changes.

--- src/mixins/dateTime.js
+++ src/mixins/dateTime.js
@@ -1,9 +1,17 @@
+import differenceInMinutes from "date-fns/differenceInMinutes";
+import parseISO from "date-fns/parseISO";
+
 export const dateTimeMixin = {
   methods: {
     getFormattedTime: function(dateIsoString) {
-      let date = new Date(dateIsoString);
+      let date = parseISO(dateIsoString);
       console.log(date);
       return date.getHours() + ":" + date.getMinutes();
+    },
+    getDifferenceInTime: function(expectedTime, aimedTime) {
+      const aimedDate = parseISO(aimedTime);
+      const expectedDate = parseISO(expectedTime);
+      return differenceInMinutes(expectedDate, aimedDate);
     }
   }
 };
